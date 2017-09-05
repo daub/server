eacb4b442dbad2035f692177edf314c60c6722dc
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2017-09-05 17:08:44 +0400

    Separate db.js

--- packages/db/lib/index.js
+++ packages/db/lib/index.js
@@ -1,43 +1,6 @@
-const { Mongoose } = require('mongoose')
-
+const Database = require('./db')
 const schemas = require('./schemas')
 
-const {
-  keys,
-  assign
-} = Object
-
-class Database {
-  constructor (schemas) {
-    const mongoose = new Mongoose()
-
-    keys(schemas).forEach(name => {
-      mongoose.model(name, schemas[name])
-    })
-
-    this.mongoose = assign(mongoose, { Promise })
-  }
-  get models () {
-    return this.mongoose.models
-  }
-  get connection () {
-    return this.mongoose.connection
-  }
-  connect (url) {
-    const options = {
-      useMongoClient: true,
-      promiseLibrary: Promise
-    }
-    return this.mongoose
-      .connect(url, options)
-      .then(() => this)
-  }
-  disconnect () {
-    return this.mongoose
-      .disconnect()
-      .then(() => this)
-  }
-}
 
 module.exports = new Database(schemas)
 module.exports.Database = Database
