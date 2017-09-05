7710918a3d454c4e2eb0a68b6a75399769c1f837
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2017-09-05 18:35:18 +0400

    Re-fix testing

--- packages/app/test/app.spec.js
+++ packages/app/test/app.spec.js
@@ -1,51 +1,39 @@
 import test from 'ava'
 
-import axios from 'axios'
-import { MongoDBServer } from 'mongomem'
+import Axios from 'axios'
 import getPort from 'get-port'
 
 import app from '../lib'
 
-// dirty for reasons
-use('/body-parser', ctx => ctx.body = ctx.request.body)
-use('/ctx-models', ctx => ctx.body = !!ctx.models)
-
-let request
-
-test.before(async t => {
-  // connect db
-  await MongoDBServer.start()
-  await MongoDBServer
-    .getConnectionString()
-    .then(url => app.db.connect(url))
-
+const Request = async (app) => {
   const port = await getPort()
   const server = app.listen(port)
+  const baseURL = `http://127.0.0.1:${port}/`
+  return Axios.create({ baseURL })
+}
 
-  request = axios.create({
-    baseURL: `http://127.0.0.1:${port}`
-  })
-})
-
-test('body parser', async t => {
+test('ctx.request.body', async t => {
   const body = { name: 'exo' }
 
-  const { data } = await request
-    .post('/body-parser', body)
+  app.use((ctx, next) => {
+    if (ctx.path !== `/${t.title}`) return next()
+    ctx.body = ctx.request.body
+  })
+
+  const request = await Request(app)
+  const { data } = await request.post(t.title, body)
 
   t.deepEqual(data, body)
 })
 
-test('ctx models', async t => {
-  const { data } = await request
-    .get('/ctx-models')
+test('ctx.models', async t => {
+  app.use((ctx, next) => {
+    if (ctx.path !== `/${t.title}`) return next()
+    ctx.body = !!ctx.models
+  })
+
+  const request = await Request(app)
+  const { data } = await request.get(t.title)
 
   t.true(data)
 })
-
-function use (path, fn) {
-  return app.use((ctx, next) => {
-    if (ctx.path !== path) return next()
-    return fn(ctx, next)
-  })
-}
