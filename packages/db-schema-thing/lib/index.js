ea4c7a9e788485e2902eccf5129d16938b1047e6
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2017-09-05 06:33:00 +0400

    Remove local mongoose

--- packages/db-schema-thing/lib/index.js
+++ packages/db-schema-thing/lib/index.js
@@ -1,4 +1,4 @@
-const { Schema } = require('@daub/mongoose')
+const { Schema } = require('mongoose')
 
 const props = require('./props')
 
--- packages/mongoose/lib/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-// Enhance with ES6 Promises
-mongoose.Promise = global.Promise
-
-module.exports = mongoose
