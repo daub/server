d6b1ab25f55a8e9d40501db9a237373373a88adf
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2017-09-06 01:04:10 +0400

    Use 'axios-serve'

--- packages/app-router-things/test/request.js
+++ packages/app-router-things/test/request.js
@@ -1,31 +1,17 @@
-const Axios = require('axios')
+const Axios = require('axios-serve')
+
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
-const getPort = require('get-port')
 const { MongoDBServer } = require('mongomem')
 const db = require('@daub/db')
 
 function Request (router) {
-  if (!(this instanceof Request)) return new Request(router)
-
   const app = new Koa()
   app.models = db.models
   app.use(bodyParser())
   app.use(router.routes())
 
-  this.app = app
-
-  this.loadAxios = loadDb()
-    .then(() => loadApp(app))
-
-  return this
-}
-
-async function loadApp (app) {
-  const port = await getPort()
-  const server = app.listen(port)
-  const baseURL = `http://localhost:${port}`
-  return Axios.create({ baseURL })
+  return new Axios(app.callback())
 }
 
 async function loadDb () {
@@ -33,23 +19,5 @@ async function loadDb () {
   const url = await MongoDBServer.getConnectionString()
   await db.connect(url)
 }
-
-const methods = [
-  'request',
-  'get',
-  'delete',
-  'head',
-  'options',
-  'post',
-  'put',
-  'patch'
-]
-
-methods.forEach(name => {
-  const call = args => axios => axios[name](...args)
-  Request.prototype[name] = function (...args) {
-    return this.loadAxios.then(call(args))
-  }
-})
 
 module.exports = Request
