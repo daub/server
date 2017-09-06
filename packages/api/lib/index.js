b17783ad9214705d2a268e0046c96e8d7563a32c
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2017-09-07 02:41:43 +0400

    Add dev. logging

--- packages/api/lib/index.js
+++ packages/api/lib/index.js
@@ -1,10 +1,15 @@
 const Koa = require('koa')
 
 const bodyParser = require('koa-bodyparser')
+const logger = require('koa-logger')
 
 const api = require('./api')
 
 const app = new Koa()
+
+if (app.env === 'development') {
+  app.use(logger())
+}
 
 app.use((ctx, next) => {
   ctx.assert(ctx.models, 501)
