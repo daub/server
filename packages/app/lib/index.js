15f7a2c704abd1631babed5416f50a98464bf75a
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2017-09-06 23:26:33 +0400

    Assign models in server level

--- packages/app/lib/index.js
+++ packages/app/lib/index.js
@@ -1,12 +1,7 @@
 const app = require('./app')
 
-const db = require('@daub/db')
-
 const thingsRouter = require('@daub/app-router-things')
-
-app.context.models = db.models
 
 app.use(thingsRouter.routes())
 
 module.exports = app
-module.exports.db = db
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const db = require('./packages/db')
 const app = require('./packages/app')
 
+app.context.models = db.models
+
 db.connect('mongodb://localhost/app')
 
 app.listen(3000)
