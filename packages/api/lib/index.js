e9273acbdc1379f01aeb2e62b795a6c365484e25
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2017-09-07 01:51:47 +0400

    Try with koa-compose

--- packages/api/lib/index.js
+++ packages/api/lib/index.js
@@ -2,12 +2,12 @@ const Koa = require('koa')
 
 const bodyParser = require('koa-bodyparser')
 
-const thingsRouter = require('@daub/api-router-things')
+const api = require('./api')
 
 const app = new Koa()
 
 app.use(bodyParser())
 
-app.use(thingsRouter.routes())
+app.use(api)
 
 module.exports = app
