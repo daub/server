23729c9d70c0fba4155c7fb8983b090f66fa92d9
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2017-09-07 01:04:05 +0400

    Rename 'app' to 'api'
    So 'app' could be used elsewhere

--- packages/api/lib/index.js
+++ packages/api/lib/index.js
@@ -2,8 +2,12 @@ const Koa = require('koa')
 
 const bodyParser = require('koa-bodyparser')
 
+const thingsRouter = require('@daub/api-router-things')
+
 const app = new Koa()
 
 app.use(bodyParser())
+
+app.use(thingsRouter.routes())
 
 module.exports = app
