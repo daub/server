c9c14207d6febc3e0300fcfd2d16602d2d1fd814
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2017-09-07 22:03:17 +0400

    Wrap into a plugin

--- packages/db-schema-thing/lib/index.js
+++ packages/db-schema-thing/lib/index.js
@@ -2,24 +2,15 @@ const Schema = require('@daub/db-schema')
 
 const props = require('./props')
 
+const sensible = require('./sensible')
+
 const schema = new Schema(props)
+
+schema.plugin(sensible)
 
 schema.methods.destroy = function () {
   return this.constructor
     .findByIdAndRemove(this._id)
 }
-
-schema.virtual('id').get(function(){
-  return this._id.toHexString();
-})
-
-schema.set('timestamps', true)
-
-schema.set('toJSON', {
-  virtuals: true,
-  transform (doc, ret) {
-    delete ret._id
-  }
-})
 
 module.exports = schema
