f7c16a1df6b2cb205a0d2df10f6ca9a8d8b49ce3
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2017-09-15 03:05:15 +0400

    Fix tests file

--- packages/api-auth/test/router.spec.js
+++ packages/api-auth/test/router.spec.js
@@ -1,9 +1,5 @@
 import test from 'ava'
 
-import {
-  isMatch
-} from 'lodash'
-
 import Request from '@daub/test-router-axios'
 
 import router from '../lib'
@@ -12,48 +8,55 @@ const request = Request(router)
 
 const { User } = request.app.context.models
 
-const validBody = { email: 'exo@exo.com', password: 'Passw0rd' }
-const invalidBody = { email: 'exo', password: 'exo' }
-const invalidPass = { email: 'exo@exo.com', password: 'exo' }
+const tokens = { invalid: 'x' }
 
-test.before(async t => {
-  await Request.loadDb()
-})
+const bodies = {
+  valid: {
+    email: 'exo@exo.com',
+    password: 'Passw0rd'
+  },
+  invalidEmail: {
+    email: '@exo',
+    password: 'Passw0rd'
+  },
+  invalidPassword: {
+    email: 'exo@exo.com',
+    password: 'x'
+  }
+}
 
 const checkErrors = (t, code, ...props) => err => {
   const { status, data } = err.response
-
   t.is(status, code)
-
   const { errors } = data || {}
-
   props.forEach(prop => t.truthy(data[prop]))
 }
 
+test.before(Request.loadDb)
+
 test('Register', async t => {
-  await t.throws(request.post('/auth/register', invalidBody))
+  await t.throws(request.post('/auth/register', bodies.invalidEmail))
     .then(checkErrors(t, 422, 'email'))
 
-  // not throws
-  await t.notThrows(request.post('/auth/register', validBody))
+  await t.notThrows(request.post('/auth/register', bodies.valid))
 
-  await t.throws(request.post('/auth/register', validBody))
+  await t.throws(request.post('/auth/register', bodies.valid))
     .then(checkErrors(t, 409, 'email'))
 })
 
 test('Login', async t => {
-  await t.throws(request.post('/auth/login', invalidBody))
+  await t.throws(request.post('/auth/login', bodies.invalidEmail))
     .then(checkErrors(t, 401))
 
-  await t.throws(request.post('/auth/login', invalidPass))
+  await t.throws(request.post('/auth/login', bodies.invalidPassword))
     .then(checkErrors(t, 401))
 
-  const login = request.post('/auth/login', validBody)
+  tokens.valid = await request.post('/auth/login', bodies.valid)
+    .then(res => {
+      t.is(res.status, 202)
 
-  await t.notThrows(login)
-
-  const { status, headers, data } = await login
-
-  t.is(status, 202)
-  t.truthy(data.accessToken)
+      const { accessToken } = res.data
+      t.true(typeof accessToken === 'string')
+      return accessToken
+    })
 })
